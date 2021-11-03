73b87c650f9d2670a56bd7e6a3402abaf6c1cdaf
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-03 12:30:51 -0700

    added semicolon rule and checkSchemeId middleware

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const Scheme = require("./scheme-model");
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,9 +8,22 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
-
-}
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const scheme = await Scheme.findById(id);
+    if (typeof scheme === "undefined") {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${id} not found`
+      });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -20,7 +35,7 @@ const checkSchemeId = (req, res, next) => {
 */
 const validateScheme = (req, res, next) => {
 
-}
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -33,10 +48,10 @@ const validateScheme = (req, res, next) => {
 */
 const validateStep = (req, res, next) => {
 
-}
+};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
