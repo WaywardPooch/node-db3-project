42f7a98af6275005fb28f9c8a41790e4abb72ee5
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-03 15:35:31 -0700

    added findSteps() to scheme model

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -93,14 +93,13 @@ async function findById(scheme_id) { // EXERCISE B
       }
   */
 
-
   const steps = await db("schemes as sc")
     .select("sc.scheme_name", "st.*")
     .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id")
     .where("sc.scheme_id", scheme_id)
     .orderBy("st.step_number", "asc");
 
-  if (steps.length > 0) {
+  if (steps.length) {
     return {
       scheme_id: Number(scheme_id),
       scheme_name: steps[0].scheme_name,
@@ -117,10 +116,9 @@ async function findById(scheme_id) { // EXERCISE B
   } else {
     return null;
   }
-
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) { // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
@@ -141,6 +139,17 @@ function findSteps(scheme_id) { // EXERCISE C
         }
       ]
   */
+
+  const steps = await db("schemes as sc")
+    .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id")
+    .select("st.step_id", "st.step_number", "st.instructions", "sc.scheme_name")
+    .where("sc.scheme_id", scheme_id)
+    .orderBy("st.step_number", "asc");
+  if (steps[0].step_id) {
+    return steps;
+  } else {
+    return [];
+  }
 }
 
 function add(scheme) { // EXERCISE D
