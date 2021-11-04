adbbb9de387a73d87cb5a2f6aa6d0dd31f9445a7
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-03 17:13:52 -0700

    completed query 3 in queries.sql

--- queries.sql
+++ queries.sql
@@ -13,4 +13,11 @@
      ON O.ShipVia = S.ID;
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+ SELECT P.ProductName, O.Quantity
+   FROM "OrderDetail" AS O
+   JOIN "Product" AS P
+     ON O.ProductID = P.ID
+  WHERE O.OrderID = 10251
+  ORDER BY P.ProductName;
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
